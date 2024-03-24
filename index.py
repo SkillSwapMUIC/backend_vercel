515a34aa0778df16195c3066c7c1bf19fa14c8d2
AP500 <and.perschin@gmail.com>
2024-03-25 00:55:35 +0700

    Update and Delete call implemented

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 import json
 from datetime import datetime
 
-from flask import request
+from flask import request, jsonify
 from sqlalchemy import func
 
 from models.question import Question
@@ -54,6 +54,49 @@ def get_question(question_id):
             "created_at": question.created_at,
         }
     )
+
+
+@app.route("/question/delete/<int:question_id>", methods=["DELETE"])
+def delete_question(question_id):
+    question = Question.query.get(question_id)
+
+    if not question:
+        return jsonify({"error": f"Question with ID {question_id} not found"}), 404
+
+    db.session.delete(question)
+    db.session.commit()
+
+    return jsonify({"message": f"Question with ID {question_id} deleted successfully"}), 200
+
+@app.route("/question/update/<int:question_id>", methods=["PUT"])
+def update_question(question_id):
+    data = request.json
+    title = data.get('title')
+    question_text = data.get('question_text')
+    user_id = data.get('user_id')
+    tags = data.get('tags')
+    created_at = data.get('created_at') 
+
+    question = Question.query.get(question_id)
+
+    if not question:
+        return jsonify({"error": f"Question with ID {question_id} not found"}), 404
+
+    if title:
+        question.title = title
+    if question_text:
+        question.question_text = question_text
+    if user_id:
+        question.user_id = user_id
+    if tags:
+        question.tags = tags
+    if created_at:
+        created_at = datetime.strptime(created_at, "%Y-%m-%dT%H:%M:%S")
+        question.created_at = created_at
+
+    db.session.commit()
+
+    return jsonify({"message": f"Question with ID {question_id} updated successfully"}), 200
 
 
 @app.route("/", methods=["GET"])
