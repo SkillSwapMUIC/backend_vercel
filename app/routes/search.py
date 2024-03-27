42afde30054dd3270ddf8f1a8175ed4672dcba8e
AP500 <and.perschin@gmail.com>
2024-03-28 00:14:07 +0700

    Searchbar api succesfully impemented with pytest

--- app/routes/search.py
+++ app/routes/search.py
@@ -1,23 +1,19 @@
-from flask import Blueprint, jsonify, request
-
-from app.models.course import Course
+from flask import Blueprint, jsonify, request, url_for, json
 from app.models.question import Question
 
 search_route = Blueprint("search", __name__)
 
+@search_route.route("/searchbar/autocomplete", methods=["GET"])
+def autocomplete():
+    search = request.args.get("search")
+    questions = Question.query.filter(Question.title.contains(search)).limit(5).all()
+    json_list = []
+    for question in questions:
+        question_link = url_for('qanda.get_question', question_id=question.id, _external=True)
+        json_list.append({"title": question.title, "id": question.id, "link": question_link})
 
-@search_route.route("/", methods=["GET"])
-def search_content():
-    keyword = request.args.get("keyword", "")
-    question_results = Question.query.filter(
-        Question.title.contains(keyword) | Question.question_text.contains(keyword)
-    ).all()
-    course_results = Course.query.filter(
-        Course.name.contains(keyword) | Course.description.contains(keyword)
-    ).all()
-
-    results = {
-        "questions": [{"id": q.id, "title": q.title} for q in question_results],
-        "courses": [{"id": c.id, "name": c.name} for c in course_results],
-    }
-    return jsonify(results), 200
+    if json_list:
+        json_data = json.dumps(json_list)
+        return json_data
+    else:
+        return jsonify({"message": "Nothing was found"}), 404
