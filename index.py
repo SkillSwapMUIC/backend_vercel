a5d33652e89965d2cd339f61b3bf0a161afbc112
jonasstDev <jonas.strehler@t-online.de>
2024-03-28 11:13:41 +0700

    made lots of functions work

--- index.py
+++ index.py
@@ -1,6 +1,25 @@
+from flask_cors import CORS
+
 from app import create_app
 
 app = create_app()
+CORS(app)
+
+
+@app.after_request
+def add_cors_headers(response):
+    response.headers["Access-Control-Allow-Origin"] = (
+        "*"  # Allow requests from any origin
+    )
+    response.headers["Access-Control-Allow-Methods"] = (
+        "GET, POST, PUT, DELETE, OPTIONS"  # Allowed HTTP methods
+    )
+    response.headers["Access-Control-Allow-Headers"] = (
+        "Content-Type, Authorization"  # Allowed request headers
+    )
+    return response
+
 
 if __name__ == "__main__":
+
     app.run(debug=True, port=5555)
